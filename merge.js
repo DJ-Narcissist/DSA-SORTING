65a323f9ed58fdc399b9a909809b1aa1ae67f17c
Christian Wise <djnarcissist@Christians-MBP.lan>
2023-12-16 11:17:44 -0500

    MERGE /MERGE SORT

--- merge.js
+++ merge.js
@@ -1,5 +1,49 @@
-function merge() {}
+function merge(arr1, arr2) {
+    let results = [];
+    let i = 0;
+    let j = 0;
 
-function mergeSort() {}
+    while (i < arr1.length && j < arr2.length) {
+        if (arr2[j] > arr1[i]) {
+            results.push(arr1[i]);
+            i++;
+        } else { 
+            results.push(arr2[j]);
+            j++;
+        }
+    }
+    while (i < arr1.length) {
+        results.push(arr1[i]);
+        i++
+    }
+    while (j < arr2.length) {
+        results.push(arr1[j]);
+        j++
+    }
 
+    return results;
+}
+
+function mergeSort(arr) { 
+    if (arr.length <= 1) return arr;
+
+    let mid = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(),mid);
+    let right = mergeSort(arr.slice(mid));
+    
+    return merge(left,right);
+}
+
+console.log(mergeSort([4, 20, 12, 10, 7, 9])); // [4, 7, 9, 10, 12, 20]
+console.log(mergeSort([0, -10, 7, 4])); // [-10, 0, 4, 7]
+console.log(mergeSort([1, 2, 3])); // [1, 2, 3]
+console.log(mergeSort([]));
+
+let nums = [
+    4, 3, 5, 3, 43, 232, 4, 34, 232, 32, 4, 35, 34, 23, 2,
+    453, 546, 75, 67, 4342, 32
+];
+
+console.log(mergeSort(nums)); // [2, 3, 3, 4, 4, 4, 5, 23, 32, 32, 34, 34, 35,
+                 //  43, 67, 75, 232, 232, 453, 546, 4342]
 module.exports = { merge, mergeSort};
