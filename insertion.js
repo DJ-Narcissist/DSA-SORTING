124a5cde9a3d332aba0b1673be883a55c5d33bc1
Christian Wise <djnarcissist@Christians-MBP.lan>
2023-12-16 11:26:29 -0500

    INSERTION

--- insertion.js
+++ insertion.js
@@ -1,3 +1,27 @@
-function insertionSort() {}
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let currVal = arr[i];
+        let j;
 
+        for (j=i-1; j>= 0 && arr[j] > currVal; j--) {
+            arr[j+1] = arr[j];
+        }
+
+        arr[j+1] = currVal;
+    }
+    return arr;
+}
+
+console.log(insertionSort([4, 20, 12, 10, 7, 9])); // [4, 7, 9, 10, 12, 20]
+console.log(insertionSort([0, -10, 7, 4])); // [-10, 0, 4, 7]
+console.log(insertionSort([1, 2, 3])); // [1, 2, 3]
+console.log(insertionSort([]));
+
+let nums = [
+    4, 3, 5, 3, 43, 232, 4, 34, 232, 32, 4, 35, 34, 23, 2,
+    453, 546, 75, 67, 4342, 32
+];
+
+console.log(insertionSort(nums)); // [2, 3, 3, 4, 4, 4, 5, 23, 32, 32, 34,
+                     //  34, 35, 43, 67, 75, 232, 232, 453, 546, 4342]
 module.exports = insertionSort;
